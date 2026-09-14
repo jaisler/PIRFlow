c5899622b970e826f5507460a862f8fff66c7b8a
João Isler <joao.isler@gmail.com>
2026-09-14 12:35:31 +0200

    Clear the plotting hang

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -2,6 +2,8 @@
 """Plot observations, prepared datasets, training histories, and flow fields."""
 
 import os
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 import pyvista as pv
